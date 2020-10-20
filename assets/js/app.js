f53be8f5d470784a6b0160a48470358d3e1a8bde
Felix DB <the2deux@gmail.com>
2020-10-20 17:22:09 -0400

    Add home section 2 and 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -82,7 +82,83 @@ function scrollAnchors(e, respond) {
 	}, 100);
 }
 
+function homeSlider() {
+    // Infinite slider that slides and stops on hover.
+    var hovered = false,
+        offset = 0,
+        offsetUnit = -0.05;
+    var viewportWidth = window.innerWidth;
+    var carousel = document.getElementById('carousel');
+    var slides = document.getElementsByClassName('home-slides');
+    var nbSlides = slides.length;
+
+    // Proportion of the screen used by one slide.
+    var slideWidthProportion = (slides[0].offsetWidth / viewportWidth) * 100;
+    // Using the concept of units to make sure that we can calculate at which slide we are
+    // without comparing percentage offset (that can be unprecise).
+    var offsetUnitsPerSlide = -(Math.round(slideWidthProportion / offsetUnit));
+
+    // Position the slide initially.
+    for (let i = 0; i < nbSlides; i++) {
+        slides[i].style.left = (slideWidthProportion * i) + '%';
+    }
+
+    function placeFirstSlides(startingPosition) {
+        slides[0].style.left = (slideWidthProportion * startingPosition) + '%';
+        slides[1].style.left = (slideWidthProportion * (startingPosition + 1)) + '%';
+    }
+
+    function placeLastSlides(startingPosition) {
+        slides[nbSlides-3].style.left = (slideWidthProportion * startingPosition) + '%';
+        slides[nbSlides-2].style.left = (slideWidthProportion * (startingPosition+1)) + '%';
+        slides[nbSlides-1].style.left = (slideWidthProportion * (startingPosition+2)) + '%';
+    }
+
+    function infiniteScroll() {
+        if (hovered) {
+            return;
+        }
+        offset += 1;
+
+        // Built using 8 slides.
+        // Once the first 2 slides aren't visible, we move them at the end.
+        if (offset === offsetUnitsPerSlide * 2) {
+            placeFirstSlides(nbSlides);
+        } else if (offset === offsetUnitsPerSlide * (nbSlides - 3)) {
+            // Just before hiding the 4th last slide, we should see the last 2 slides + 2 first slides
+            // Reposition the first 2 slides in the "beginning" of their container
+            placeFirstSlides(0);
+            // Position the last 3 slides "before" the beginning of their container.
+            placeLastSlides(-3);
+            // Slide the container the see before it. The first visible slide should be the second last.
+            offset = -offsetUnitsPerSlide*3;
+        } else if (offset === 0) {
+            // Once we are back at the beginning, we can reposition the last slides at the end
+            placeLastSlides(nbSlides - 3);
+        }
+
+        // The translation gives the impression of movement.
+        carousel.style.transform = 'translateX('+(offset * offsetUnit)+'%)';
+    }
+
+    // Translate the container
+    setInterval(infiniteScroll, 10);
+
+    carousel.addEventListener("mouseenter", function() {
+        hovered = true;
+    });
+
+    carousel.addEventListener("mouseleave", function() {
+        hovered = false;
+    });
+}
+
+function home() {
+    homeSlider();
+}
+
 (function() {
     scrollTo();
     header();
+    home();
 })();
--- content/_index.md
+++ content/_index.md
@@ -1,6 +1,84 @@
 ---
 title: ""
 date: 2019-03-17T18:18:43Z
-h1: is a digital marketing agency that specializes in client and lead aquisitions;
+h1: is a digital marketing agency that specializes in client and lead aquisitions
+portfolio:
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/deux-par-deux-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/dormani-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/cna-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/inbeat-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/dormani-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/deux-par-deux-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/inbeat-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/cna-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/deux-par-deux-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/dormani-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/cna-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/inbeat-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/dormani-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/deux-par-deux-portfolio.png
+  - images:
+    - url: '#'
+      title: 'Test'
+      src: /img/inbeat-portfolio.png
+    - url: '#'
+      title: 'Test'
+      src: /img/cna-portfolio.png
+section3:
+  title: We do
+  services:
+    - name: Search engine optimization
+      url: '#'
+    - name: Digital Marketing
+      url: '#'
+    - name: Consulting + Advising
+      url: '#'
+    - name: Digital Strategy Building
+      url: '#'
+    - name: User Interface & Experience
+      url: '#'
+    - name: Software + Web Developpement
+      url: '#'
+    - name: Design & Branding Systems
+      url: '#'
+  subtitle: '& believe in strategic creativity based on data and analytics.'
+  description: >-
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing 
 ---
 
